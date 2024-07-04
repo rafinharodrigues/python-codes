6bb2d0d4e1d6e0ee59fbb55edac70a49bababa78
Rafael Rodrigues <rafael.rodrigues@great.local.ufc.br>
2024-07-04 12:23:30 -0300

    Exercício de funções 02

---
+++ secao_4/funcoes_02.py
@@ -0,0 +1,49 @@
+### Função que duplica o número recebido
+# Modo um
+def duplicate1(num):
+    return num * 2
+
+# Modo dois
+def duplicate2(num):
+    return f'{num}'*2
+
+### Função que triplica o número recebido
+# Modo um
+def triplicate1(num):
+    return num * 3
+
+# Modo dois
+def triplicate2(num):
+    return f'{num}'*3
+
+
+### Função que quadruplica o número recebido
+# Modo um
+def quadruplicate1(num):
+    return num * 4
+
+# Modo dois
+def quadruplicate2(num):
+    return f'{num}'*4
+
+
+# print(duplicate1(2),duplicate2(2))
+# print(triplicate1(2),triplicate2(2))
+# print(quadruplicate1(2),quadruplicate2(2))
+
+# Mesma função, mas com menos linhas
+def calculate(num):
+    def mode(mode):
+        if mode == 'duplicate':
+            return num * 2
+        elif mode == 'triplicate':
+            return num * 3
+        elif mode == 'quadruplicate':
+            return num * 4
+        return 'Modo inválido.'
+
+    return mode
+
+numero = calculate(2)
+print(numero('quadruplicate'))
+
